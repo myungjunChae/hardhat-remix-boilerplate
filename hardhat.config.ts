9bb2381f82aa782b60cc274d2e2cd722eb7977bb
ono <cmj9597@naver.com>
2022-04-26 11:32:47 +0900

    Update hardhat.config.ts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,23 +1,31 @@
 import { task } from "hardhat/config";
 import "@nomiclabs/hardhat-waffle";
 import "@nomiclabs/hardhat-web3";
+import "@nomiclabs/hardhat-etherscan";
 import "@typechain/hardhat";
 import { HardhatUserConfig } from "hardhat/types";
 
 import { execSync } from "child_process";
-import { env } from "process";
+import dotenv from "dotenv";
+dotenv.config();
 
-// todo : 추후에 테스트넷 및 메인넷을 추가할 때, 사용하기 위함
-// const ALCHEMY_API_KEY = "KEY";
-// const ROPSTEN_PRIVATE_KEY = "YOUR ROPSTEN PRIVATE KEY";
+const INFURA_END_POINT = process.env.INFURA_END_POINT;
+const WALLET_PRIVATE_KEY = process.env.WALLET_PRIVATE_KEY ?? "";
+const ETHER_SCAN_API = process.env.ETHER_SCAN_API ?? "";
 
 const config: HardhatUserConfig = {
-	solidity: "0.7.3",
+	solidity: "0.8.7",
 	networks: {
-		// ropsten: {
-		//     url: `https://eth-ropsten.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
-		//     accounts: [`${ROPSTEN_PRIVATE_KEY}`],
-		// },
+		rinkeby: {
+			url: INFURA_END_POINT,
+			accounts: [WALLET_PRIVATE_KEY], //todo : 배포할 계정으로 변경해야함
+		},
+	},
+	//https://hardhat.org/plugins/nomiclabs-hardhat-etherscan.html
+	etherscan: {
+		apiKey: {
+			rinkeby: ETHER_SCAN_API,
+		},
 	},
 };
 
